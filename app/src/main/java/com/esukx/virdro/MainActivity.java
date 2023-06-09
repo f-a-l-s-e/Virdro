86902593212acc98d99b484a4cedb21b1ea75c2d
KouXana <kouxana0107@gmail.com>
2023-06-09 10:33:22 +0900

    first commit

---
+++ app/src/main/java/com/esukx/virdro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.esukx.virdro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
